ea7aafd74dafe56bb33974c4aa1e8f34e9f85eb1
Saad Elbeleidy <sbeleidy90@gmail.com>
2016-04-16 23:53:52 +0200

    Update executable and add tests

--- travis-runner.sh
+++ travis-runner.sh
@@ -1,8 +1,8 @@
+npm test
 # if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 # then
     # API Docs Setup & Deploying
     echo Setting up API Docs and Deploying to Firebase
-    npm install -g apidoc
     npm run apidoc
     firebase deploy --token "$FIREBASE_TOKEN"
 # fi
