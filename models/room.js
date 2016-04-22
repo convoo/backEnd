52cca8f5bef3c2ec4ca8f32bc6147049cc61ebbb
Saad Elbeleidy <sbeleidy90@gmail.com>
2016-04-23 01:04:31 +0200

    Add room and initial test

--- models/room.js
+++ models/room.js
@@ -19,67 +19,130 @@
 
 var r = require("rethinkdb");
 var log = require('./log.js');
+var User = require('./user');
+var slug = require('slug');
 var table = r.db(process.env.RETHINK_DB).table("rooms");
 
 
 //----------------------------- ADD ROOM
-exports.add = function (userID, roomName) {
-
-        console.log(userID);
-        console.log(roomName);
+exports.add = function (userID, roomData) {
         // connect
         var c = r.connect({host: process.env.RETHINK_HOST, port: process.env.RETHINK_PORT});
+        var today = new Date();
 
-        c.then(function(conn){
-            // Insert user
-            table.insert({
-                name: roomName,
-                // TODO: Create function for URL shortener for room URL
-                // TODO: Add short URL for each room to database
-                // TODO: Handle rooms with the same name by prepending the user name of the creator and postpending a version number of the user makes more that one room with the same name
-                created_at: r.now()
-              // TODO: Add room created by UserId
+        return c.then(function(conn){
+            return User.get(userID).then(function(user){
+                // Insert room
+                return table.insert({
+                    title: roomData.title,
+                    canJoin: roomData.canJoin,
+                    sharedUrl: roomData.url,
+                    description: roomData.description,
+                    // slug: slug(roomData.title) + "-@" + user.user_name + "-" + today.getUTCDate + "-" + today.toLocaleString("en-us", {month: "short"}) + "-" + today.getFullYear,
+                    // TODO: Create function for URL shortener for room URL
+                    // TODO: Add short URL for each room to database
+                    // TODO: Handle rooms with the same name by prepending the user name of the creator and postpending a version number of the user makes more that one room with the same name
+                    created_at: r.now(),
+                    created_by: user.id
+                })
+                // TODO: Add room created by UserId
+                .run(conn)
+                // Catch any errors
+                .catch(function(err){
+                    console.log(err);
+                })
+                // Close the connection
+                .then(function(result){
+                    conn.close();
+                    return result;
+                })
+            });
+        });
 
-            })
+        // make log
+        // log.add('New Room', userID, roomName);
+    };
+
+//----------------------------- DELETE ROOM
+exports.get = function (roomID) {
+    var c = r.connect({ host: process.env.RETHINK_HOST, port: process.env.RETHINK_PORT });
+    return c.then(function (conn) {
+        return table.get(roomID)
             .run(conn)
             // Catch any errors
             .catch(function(err){
                 console.log(err);
             })
-            // Close the connection
-            .finally(function(){
+            .then(function(result){
                 conn.close();
-            });
-        });
+                return result;
+            })
+    });
+};
 
-        // make log
-        log.add('New Room', userID, roomName);
-    };
+exports.getBySlug = function (roomSlug) {
+    var c = r.connect({ host: process.env.RETHINK_HOST, port: process.env.RETHINK_PORT });
+    return c.then(function (conn) {
+        return table.filter({slug: roomSlug})
+            .run(conn)
+            // Catch any errors
+            .catch(function(err){
+                console.log(err);
+            })
+            .then(function(result){
+                conn.close();
+                return result;
+            })
+    });
+};
 
 //----------------------------- DELETE ROOM
 exports.del = function (userID, roomID) {
         // connect
-        var c = r.connect({host: process.env.RETHINK_HOST, port: process.env.RETHINK_PORT});
+    var c = r.connect({host: process.env.RETHINK_HOST, port: process.env.RETHINK_PORT});
 
-        c.then(function(conn){
-            // Insert user
-            table.get(roomID).update({
-                deleted_at: r.now()
-                // TODO: Add room closed by UserId
-            })
-            .run(conn)
-            // Catch any errors
-            .catch(function(err){
-                console.log(err);
-            })
-            // Close the connection
-            .finally(function(){
-                conn.close();
-            });
+    c.then(function(conn){
+        // Insert user
+        table.get(roomID).update({
+            deleted_at: r.now()
+            // TODO: Add room closed by UserId
+        })
+        .run(conn)
+        // Catch any errors
+        .catch(function(err){
+            console.log(err);
+        })
+        // Close the connection
+        .finally(function(){
+            conn.close();
         });
+    });
 
-        // make log
-        log.add('Delete Room ' + roomID, userID, "/");
-    };
+    // make log
+    // log.add('Delete Room ' + roomID, userID, "/");
+};
 
+exports.forceDelete = function (roomID) {
+    // connect
+    var c = r.connect({host: process.env.RETHINK_HOST, port: process.env.RETHINK_PORT});
+
+    return c.then(function(conn){
+        // Insert user
+        return table.get(roomID)
+        .delete({
+            durability: "hard",
+            returnChanges: true
+        })
+        .run(conn)
+        // Catch any errors
+        .catch(function(err){
+            console.log(err);
+        })
+        // Close the connection
+        .then(function(result){
+            conn.close();
+            return result;
+        });
+    });
+};
 //----------------------------- END
---
+++ test/models/room.test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var should = require('should');
+var Token = require('../../helpers/token');
+
+
+describe('Room Model', function () {
+    require('dotenv').config();
+    var User = require('../../models/user');
+    var userType = 'Guest',
+        userID = Token.makeID(),
+        socketID = Token.makeID();
+    var Room = require('../../models/room');
+    var roomData = {
+        title: "This is a test title",
+        canJoin: true,
+        description: "This is a very long description about the room.",
+    }
+
+
+    before(function () {
+        User.add('Guest', userID, socketID);
+    });
+
+    after(function (){
+        User.forceDelete(userID);
+    })
+
+    // Tests several functions at once
+    // TODO: Figure out how to separate and test in unit tests
+    it('should add, get and force delete a room', function () {
+        Room.add(userID, roomData)
+        .then(function(result){
+            return Room.get(userID);
+        }).then(function(result){
+            result.should.be.an.Object();
+            result.created_by.should.equal(userID);
+            return result.id;
+        }).then(function(addedID){
+            Room.forceDelete(addedID);
+            return addedID;
+        }).then(function(addedID){
+            return Room.get(addedID);
+        }).then(function(result){
+            should.not.exist(result);
+        });
+    });
+});
