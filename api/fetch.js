97efd5299ae84732771d4a41d5f1ea572cb57ca0
Saad Elbeleidy <sbeleidy90@gmail.com>
2016-04-22 03:39:53 +0200

    Add apidoc for fetch and update old ones

--- api/fetch.js
+++ api/fetch.js
@@ -3,7 +3,20 @@
 
 var Fetch = require('../helpers/fetch');
 
-
+/**
+ * @api {get} /fetch/?url Request details about a url
+ * @apiName fetchUrlDetails
+ * @apiGroup Fetch
+ * @apiPermission Public
+ * @apiVersion 0.0.1
+ *
+ * @apiParam {String} url The encoded url to get details about
+ *
+ * @apiSuccess {String} title The title of the url
+ * @apiSuccess {String} description The description of the url
+ * @apiSuccess {String} image The url of the main image for the url
+ * @apiSuccess {Array} tags An array of strings that are tags/keywords for the url
+ */
 exports.url = function (req, res) {
     Fetch.url(req.query.url)
         .then(function(data){
