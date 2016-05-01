9b69481193c60f6bec26adaa0e8cbe5ef5323550
@bkawk <email.will.in.china@gmail.com>
2016-05-01 20:27:13 +0800

    rate limiting

--- sockets/events.js
+++ sockets/events.js
@@ -10,38 +10,58 @@ module.exports = function (io) {
     //-- AUTH CONNECT & DISCONNECT
     io.on('connect', function (socket, msg) {
 
+
         var sockets = require('./controllers');
 
         socket.emit('hello', {message: "Hello from Convoo!"})
 
         socket.on('jwt', function (msg) {
+            rateLimit(100);
             sockets.auth.jwt(socket, io, msg);
         });
 
         socket.on('twitterRequestToken', function (msg) {
+            rateLimit(100);
             sockets.auth.twitterRequestToken(socket, io, msg);
         });
 
         socket.on('disconnect', function () {
+            rateLimit(100);
             sockets.auth.disconnect(socket);
         });
 
         socket.on('latency', function (startTime, callBack) {
+            rateLimit(100);
             callBack(startTime);
         });
 
         socket.on('editProfile', function (msg) {
+            rateLimit(100);
             sockets.user.editProfile(socket, msg);
         });
 
         socket.on('getProfile', function (msg) {
+            rateLimit(100);
             sockets.user.getProfile(socket, io, msg);
-            console.log(socket.id);
         });
 
         socket.on('addRoom', function (msg) {
+            rateLimit(100);
             sockets.room.add(msg);
         });
+
+
+        function rateLimit(max){
+            if(Date.now() - socket.lastEventTimestamp < max){
+                socket.disconnect();
+            return;
+            }
+        }
+        socket.lastEventTimestamp = Date.now();
+
+
+
+
 
 
 
